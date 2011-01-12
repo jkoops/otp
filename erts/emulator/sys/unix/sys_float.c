3beeabd32b2639da5af94e0704e9e26d88e55e02
Serge Aleynikov <serge@aleynikov.org>
2011-01-12 00:44:08 -0500

    new float_to_list/2
    Attached please find a patch that adds a new float_to_list/2 BIF.  The
    patch was created off of the master branch of https://github.com/erlang/otp.
    
    This BIF solves a problem of float_to_list/1 that doesn't allow
    specifying the number of digits after the decimal point when formatting
    floats.
    
    	float_to_list(Float, Options) -> string()
    
    	Float = float()
    	Options = [Option]
    	Option = {precision, Precision::integer()} | compact
    
    	Text representation of a float formatted using given options
    
    	Returns a string which corresponds to the text
    	representation of Float using fixed decimal point formatting.
             When precision option is specified
             the returned value will contain at most Precision number of
             digits past the decimal point.  When compact option is provided
             the trailing zeros at the end of the list are truncated.
    
    	1> float_to_list(7.12, [{precision, 4}]).
    	"7.1200"
    	2> float_to_list(7.12, [{precision, 4}, compact]).
    	"7.12"
    
    Documentation and test cases are updated to reflect the changes.

--- erts/emulator/sys/unix/sys_float.c
+++ erts/emulator/sys/unix/sys_float.c
@@ -764,6 +764,22 @@ sys_double_to_chars(double fp, char *buf)
     return s-buf; /* i.e strlen(buf) */
 }
 
+int
+sys_double_to_chars_fixed(double fp, char *buf, int sz, int precision, int compact)
+{
+    char *s = buf;
+
+    int n = snprintf(buf, sz, "%.*f", precision, fp);
+    /* Search upto decimal point */
+    if (*s == '+' || *s == '-') s++;
+    while (ISDIGIT(*s)) s++;
+    if (*s == ',') *s++ = '.'; /* Replace ',' with '.' */
+    if (compact)
+        for(s = buf + n - 1; *s == '0' && s > buf && *(s-1) != '.'; s--)
+            n--;
+    return n; /* i.e strlen(buf) */
+}
+
 /* Float conversion */
 
 int
